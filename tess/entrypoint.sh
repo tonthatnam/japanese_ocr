2d61894657a691e0a3a4049ded58432c87b34c5a
tonthatnam <thatnamdn@gmail.com>
2021-03-06 18:58:22 +0900

    update docker

--- tess/entrypoint.sh
+++ tess/entrypoint.sh
@@ -1,1 +1,5 @@
 echo "export TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata/" >> ~/.profile && source ~/.profile
+#download jpn_best model
+wget https://github.com/tesseract-ocr/tessdata_best/raw/master/jpn.traineddata -O $TESSDATA_PREFIX/jpn_best.traineddata
+#change jpn_best to foramat for training LSTM
+combine_tessdata -e $TESSDATA_PREFIX/jpn_best.traineddata ~/tess/jpn_best.lstm
