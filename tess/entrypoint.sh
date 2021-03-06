897ea53a7f30dc7a6fc09dfc416d0cb205fc21b1
tonthatnam <thatnamdn@gmail.com>
2021-03-06 21:54:01 +0900

    update docker

--- tess/entrypoint.sh
+++ tess/entrypoint.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
 echo "export TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata/" >> ~/.profile && source ~/.profile
 #download jpn_best model
+wget https://github.com/tesseract-ocr/tessdata/raw/master/jpn.traineddata -P $TESSDATA_PREFIX
+wget https://github.com/tesseract-ocr/tessdata_best/raw/master/eng.traineddata -O $TESSDATA_PREFIX/eng_best.traineddata
 wget https://github.com/tesseract-ocr/tessdata_best/raw/master/jpn.traineddata -O $TESSDATA_PREFIX/jpn_best.traineddata
+wget https://github.com/tesseract-ocr/tessdata_best/raw/master/jpn_vert.traineddata -P $TESSDATA_PREFIX
 #change jpn_best to foramat for training LSTM
-combine_tessdata -e $TESSDATA_PREFIX/jpn_best.traineddata ~/tess/jpn_best.lstm
+combine_tessdata -e $TESSDATA_PREFIX/jpn_best.traineddata jpn_best.lstm
+pwd
+ls
+#tesseract --list-langs
+tesseract -l jpn_best tests/test1.png stdout
